2278bde0831ec635ef8bbc25ec185a3653ca3e1f
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-02 15:42:40 -0300

    render SearchBar in App

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from "./search_bar"
 
 const App = () => {
   return (
     <div className="app">
       <div className="left-scene">
-        <div className="search-bar"></div>
+        <SearchBar />
         <div className="gif"></div>
       </div>
       <div className="right-scene">
