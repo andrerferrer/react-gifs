c702976975b3545f989e7d18b398b60db2c3d7dd
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-02 15:29:42 -0300

    create app.jsx

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="left-scene">
+        <div className="search-bar"></div>
+        <div className="gif"></div>
+      </div>
+      <div className="right-scene">
+        <div className="giflist"></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
