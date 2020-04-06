c5ac15c4c0a94feff2513f6a9778eeda2373222f
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 18:32:43 -0300

    Refactor search bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import giphy from "giphy-api";
 
 class SearchBar extends Component {
-    update = (event) => {
-        const query = event.currentTarget.value;
-        this.props.search(query);
-    }
-    render() {
-        return (
-           <input type="text" className="form-control form-search"
-               onChange={this.update}
-           />
-        );
-    }
+  update = (event) => {
+    const query = event.currentTarget.value;
+    const { search } = this.props;
+    search(query);
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.update}
+      />
+    );
+  }
 }
 
 export default SearchBar;
