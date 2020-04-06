d9b6f5ab9c64352ff51da90cea2320cbbf517142
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 19:24:40 -0300

    refactor app jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,46 +7,47 @@ import Gif from "./gif";
 import GifList from "./gif-list";
 
 class App extends Component {
-
+  
   constructor(props) {
     super(props);
-
+    
     this.state = {
       gifs: [],
       selectedGifId: "chVgEkHr9oYz4ZPGeU"
     }
-
   }
-
+  
   changeSelectedGif = (newGifId) => {
     this.setState({ selectedGifId: newGifId });
   }
-
+  
   searchGif = (query) => {
     giphy().search({q: query, limit: 10})
-           .then( (res) => {
-              this.setState({
-                gifs: res.data
-              })
-            });      
+    .then( (res) => {
+      // console.log(res)
+      this.setState({
+        gifs: res.data
+      })
+    });
   }
-
+  
   render() {
-
+    
     return (
       <div className="app">
-        <div className="left-scene">
-          <SearchBar search={this.searchGif} />
-          <div className="selected-gif">
-            <Gif id={this.state.selectedGifId} />
-          </div>
-        </div>
-        <div className="right-scene">
-          <GifList changeSelectedGif={this.changeSelectedGif} gifIds={this.state.gifs} />
-        </div>
+      <div className="left-scene">
+      <SearchBar search={this.searchGif} />
+      <div className="selected-gif">
+      <Gif id={this.state.selectedGifId} />
       </div>
-    );
-  }
-};
-
-export default App;
+      </div>
+      <div className="right-scene">
+      <GifList changeSelectedGif={this.changeSelectedGif} gifIds={this.state.gifs} />
+      </div>
+      </div>
+      );
+    }
+  };
+  
+  export default App;
+  
