21c8b8c877bb0f79d1f5cd91945b3d47a2d7ee42
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 19:08:18 -0300

    improve gif list to pass behavior on

--- src/components/gif-list.jsx
+++ src/components/gif-list.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Gif from "./gif";
 
 class GifList extends Component {
-    renderList = () => this.props.gifIds.map( (gif) => <Gif id={gif.id} /> )
-
-    render() {
-        return (
-            <div className="gif-list">
-                {this.renderList()}
-            </div>
-        );
-    }
+  renderList = () => {
+    const { gifIds, changeSelectedGif } = this.props
+    return gifIds.map( (gif) => { 
+      return <Gif id={gif.id} changeSelectedGif={changeSelectedGif} /> 
+    });
+  }
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
+  }
 }
 
 export default GifList;
