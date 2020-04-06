e288b9ba2a87280b5804782a198e5245c842e9e1
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 19:25:12 -0300

    Make searchbar only fetch when user stops typing

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeout: null
+    };
+  }
+
   update = (event) => {
+    const { timeout } = this.state
     const query = event.currentTarget.value;
     const { search } = this.props;
-    search(query);
+
+    console.log(timeout)
+
+    clearTimeout(timeout)
+
+    this.state.timeout = setTimeout(() => {
+      search(query);
+    }, 1000)
   }
 
   render() {
