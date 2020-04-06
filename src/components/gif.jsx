1e04257225f3c46db4071909532bd4b6dc2703c6
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 19:08:05 -0300

    improve gif to change selected gif

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Gif = (props) => {
-  // const id = props.id
-  const { id } = props;
-  const src = `https://media.giphy.com/media/${id}/giphy.gif`;
-  return (
-    <img src={src} alt="" className="gif" />
-  );
-};
+class Gif extends Component {
+  clicked = () => {
+    const { changeSelectedGif, id } = this.props
+    changeSelectedGif(id)
+  }
+
+  render = () => {
+    const { id } = this.props;
+    const src = `https://media.giphy.com/media/${id}/giphy.gif`;
+
+    return (
+      <img
+        src={src}
+        alt=""
+        className="gif"
+        onClick={this.clicked}
+      />
+    );
+  }
+}
 
 export default Gif;
